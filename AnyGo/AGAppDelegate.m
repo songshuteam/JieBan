fb9e087ea3f396173123ba50d9e7daa9ddc96ec9
Wingle Wong <winglewong@Wingles-iMac.local>
2014-08-28 14:48:39 +0800

    Change State bar text Color

--- AnyGo/AGAppDelegate.m
+++ AnyGo/AGAppDelegate.m
@@ -33,7 +33,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     
     [self umengTrack];
     
